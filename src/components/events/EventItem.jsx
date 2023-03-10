7170584d5bb6c714e0b61879cfaaa8eeb4ebe950
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2023-03-10 09:39:54 +0700

    Buttons and icons

--- src/components/events/EventItem.jsx
+++ src/components/events/EventItem.jsx
@@ -1,4 +1,8 @@
 import Link from 'next/link';
+import AddressIcon from '../icons/AddressIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
+import DateIcon from '../icons/DateIcon';
+import Button from '../ui/button';
 import classes from './event-item.module.css';
 
 function EventItem(props) {
@@ -17,14 +21,21 @@ function EventItem(props) {
       <div className={classes.summary}>
         <h2>{title}</h2>
         <div className={classes.date}>
+          <DateIcon/>
           <time>{readableDate}</time>
         </div>
         <div className={classes.address}>
+          <AddressIcon/>
           <address>{formattedAddress}</address>
         </div>
       </div>
       <div className={classes.actions}>
-        <Link href={exploreLink}>Explore Event</Link>
+        <Button link={exploreLink}>
+          Explore Event
+          <span className={classes.icon}>
+            <ArrowRightIcon/>
+          </span>
+        </Button>
       </div>
     </div>
   </li>
